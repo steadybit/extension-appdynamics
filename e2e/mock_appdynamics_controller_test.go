2941ae637f11c921bc14484ad69400ae6c6bd194
antoine.choimet <12182686+achoimet@users.noreply.github.com.>
2025-05-14 17:50:45 +0200

    fix: integration test

--- e2e/mock_appdynamics_controller_test.go
+++ e2e/mock_appdynamics_controller_test.go
@@ -35,8 +35,8 @@ func createMockAppDynamicsController() *mockServer {
 
 	mock := &mockServer{http: &server, state: "CLEAR"}
 	mux.Handle("/controller/rest/applications", handler(mock.viewApplications))
-	mux.Handle("/controller/alerting/rest/v1/applications/1/health-rules?output=JSON", handler(mock.viewHealthRules))
-	mux.Handle("/controller/alerting/rest/v1/applications/2/health-rules?output=JSON", handler(mock.viewHealthRules))
+	mux.Handle("/controller/alerting/rest/v1/applications/1/health-rules", handler(mock.viewHealthRules))
+	mux.Handle("/controller/alerting/rest/v1/applications/2/health-rules", handler(mock.viewHealthRules))
 	return mock
 }
 
